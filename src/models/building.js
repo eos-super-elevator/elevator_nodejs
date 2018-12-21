09cdf6d67e4a52dec8fa5ce9201d8e63201ac4f5
fbillard <fbillard01@gmail.com>
2018-12-21 12:45:40 +0100

    [+] JSON building structure on /building route

---
+++ src/models/building.js
@@ -0,0 +1,43 @@
+const building = {
+    "floor_height": 4,
+    "floors": [
+        {
+            "id": 1,
+            "restricted_area": false
+        },
+        {
+            "id": 2,
+            "restricted_area": false
+        },
+        {
+            "id": 3,
+            "restricted_area": false
+        },
+        {
+            "id": 4,
+            "restricted_area": false
+        },
+        {
+            "id": 5,
+            "restricted_area": false
+        },
+        {
+            "id": 6,
+            "restricted_area": false
+        },
+        {
+            "id": 7,
+            "restricted_area": false
+        },
+        {
+            "id": 8,
+            "restricted_area": false
+        },
+        {
+            "id": 9,
+            "restricted_area": false
+        }
+    ]
+};
+
+export default building;
--- src/routes/elevator.js
+++ src/routes/elevator.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import building from '../models/building';
 
 const fs = require('fs');
 const showdown = require('showdown');
@@ -13,6 +14,15 @@ router.get('/', function (req, res) {
         if (err) throw err;
         res.send(converter.makeHtml(data));
     });
+});
+
+/**
+ * Get the JSON building description
+ */
+router.get('/building', function (req, res) {
+    console.log('Returned the building structure');
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(building));
 });
 
 export default router;
