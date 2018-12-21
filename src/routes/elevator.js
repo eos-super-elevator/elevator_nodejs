31db56fecb3a07bfa054d177bc878aa34211975d
fbillard <fbillard01@gmail.com>
2018-12-21 12:37:34 +0100

    [+] Api documentation (serve README.md) on index route

--- src/routes/elevator.js
+++ src/routes/elevator.js
@@ -1,8 +1,18 @@
 import express from 'express';
+
+const fs = require('fs');
+const showdown = require('showdown');
 const router = express.Router();
 
+/**
+ * API documentation
+ */
 router.get('/', function (req, res) {
-    res.send('EOS Super Elevator v1.0');
+    const converter = new showdown.Converter();
+    fs.readFile('README.md', 'utf-8', function (err, data) {
+        if (err) throw err;
+        res.send(converter.makeHtml(data));
+    });
 });
 
 export default router;
